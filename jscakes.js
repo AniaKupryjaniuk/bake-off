50c86ed7d64b04c64f4648c12925309a82197e6d
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-02-19 20:42:12 +0100

    logo preview, multiple js attempt

---
+++ jscakes.js
@@ -0,0 +1,58 @@
+const link = "https://spreadsheets.google.com/feeds/list/1b8lIZPv5iZJX1LIgl9cxJ43cRnQ_ZgZbaOjARC0J5ts/od6/public/values?alt=json";
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData() {
+    fetch(link)
+    .then(res => res.json())
+    .then (handleData);
+}
+
+function handleData(data) {
+    const myData = data.feed.entry;
+    console.log("myData - console");
+    console.log(myData);
+    myData.forEach(showData);
+}
+
+function showData(singleRecipe) {
+    const template = document.querySelector("#templ").content;
+    const clone = template.cloneNode(true);
+    const category = singleRecipe.gsx$category.$t
+    clone.querySelector("h3").textContent = singleRecipe.gsx$name.$t;
+
+    if (category === "cake"){
+        /*const Img = getImageName(singleRecipe.gsx$name.$t);
+        clone.querySelector(".image").src = Img;*/
+        document.querySelector("#cakes").appendChild(clone);
+    }
+
+}
+
+/*function getImageName(imageName) {
+    const base = ;
+    return base + imageName + ".jpg";
+}*/
+
+
+
+
+
+//back to top button
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    var topBtn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+// click on the button = scroll to the top
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
