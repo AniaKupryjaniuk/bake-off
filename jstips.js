faae6709b1907fd0d03c9c1c2b61bcd56848e369
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-02-20 00:28:09 +0100

    tips adjustments

--- jstips.js
+++ jstips.js
@@ -18,7 +18,21 @@ function showData(singleTipsCateg) {
     const template = document.querySelector("#templ").content;
     const clone = template.cloneNode(true);
     clone.querySelector("h3").textContent = singleTipsCateg.gsx$name.$t;
+    clone.querySelector(".description").textContent = singleTipsCateg.gsx$description.$t;
+    clone.querySelector(".category").textContent = singleTipsCateg.gsx$category.$t;
+    console.log(singleTipsCateg.gsx$category.$t)
 
-    document.querySelector("#tips").appendChild(clone);
+    if (singleTipsCateg.gsx$category.$t == "For improvement seekers"){
+        clone.querySelector(".category").classList.add("impr");
+    }
+    if (singleTipsCateg.gsx$category.$t == "For improvisators"){
+        clone.querySelector(".category").classList.add("improv");
+    }
+    if (singleTipsCateg.gsx$category.$t == "For food waste fighters"){
+        clone.querySelector(".category").classList.add("fight");
+        /*clone.querySelector(".fight").textContent = singleTipsCateg.gsx$category.$t;*/
+    }
+
+    document.querySelector("#parent").appendChild(clone);
 
 }
