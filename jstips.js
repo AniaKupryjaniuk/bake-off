53e243791619ed395fe2abe20cd1ab9dd6bf58dd
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-02-20 01:31:08 +0100

    tips copy

--- jstips.js
+++ jstips.js
@@ -36,3 +36,23 @@ function showData(singleTipsCateg) {
     document.querySelector("#parent").appendChild(clone);
 
 }
+
+//back to top button
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    var topBtn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+// click on the button = scroll to the top
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
