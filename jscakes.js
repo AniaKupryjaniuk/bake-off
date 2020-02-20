472a514c61b7196c41cefda133416d4198ecc28d
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-02-20 13:31:39 +0100

    images updated

--- jscakes.js
+++ jscakes.js
@@ -3,8 +3,8 @@ window.addEventListener("DOMContentLoaded", getData);
 
 function getData() {
     fetch(link)
-    .then(res => res.json())
-    .then (handleData);
+        .then(res => res.json())
+        .then(handleData);
 }
 
 function handleData(data) {
@@ -20,22 +20,37 @@ function showData(singleRecipe) {
     const category = singleRecipe.gsx$category.$t
     clone.querySelector("h3").textContent = singleRecipe.gsx$name.$t;
 
-    if (category === "cake"){
-        /*const Img = getImageName(singleRecipe.gsx$name.$t);*/
+    if (category === "cake") {
         clone.querySelector(".image").src = "images/cakes/" + singleRecipe.gsx$picture.$t;
         clone.querySelector(".type").textContent = singleRecipe.gsx$type.$t;
         document.querySelector("#parent").appendChild(clone);
 
     }
-
 }
 
-/*function getImageName(imageName) {
-    const base = ;
-    return base + imageName + ".jpg";
-}*/
+// Get the modal
+const modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
 
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
 
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
